17a221bc6d56d94adc403189a687f2c228b46d1c
wrdecurtins <william.decurtins@gmail.com>
2024-11-16 16:44:52 -0600

    update lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,19 +8,22 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', '@stylistic/js'],
+  plugins: ['react-refresh', '@stylistic/js', 'newline-destructuring'],
   rules: {
+    "indent": ["error", 2],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     "object-curly-newline": ["error", {
+      "ObjectExpression": { "multiline": true, "minProperties": 3 },
       "ObjectPattern": { "multiline": true, "minProperties": 2 },
       "ImportDeclaration": { "multiline": true, "minProperties": 2 },
       "ExportDeclaration": { "multiline": true, "minProperties": 2 },
    }],
    "object-curly-spacing": ["error", "always"],
    "object-property-newline": ['error'],
+   'newline-destructuring/newline': 'error',
    "space-before-blocks": ["warn", "always"],
    "semi-style": ["warn", "last"],
    "semi": ["warn", "always"]
